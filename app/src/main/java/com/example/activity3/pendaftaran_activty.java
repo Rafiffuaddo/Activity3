0880556d4be4c678710c67817dbacebfcad2b1e9
rafiffuaddo <rafiffuad3108@gmail.com>
2021-04-01 18:01:55 +0700

    fiff

---
+++ app/src/main/java/com/example/activity3/pendaftaran_activty.java
@@ -0,0 +1,14 @@
+package com.example.activity3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pendaftaran_activty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pendaftaran_activty);
+    }
+}
